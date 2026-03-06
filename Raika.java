b3e7caffb02cd661a3832c888a9498b23ea75464
Aryanny Galvão <aryanny.silva@dcx.ufpb.br>
2026-03-06 11:16:50 -0300

    enviar mensagem

--- Raika.java
+++ Raika.java
@@ -2,4 +2,7 @@ public class Calculadora {
     public static int soma(int a, int b){
         return a + b;
     }
+    public static void main(String[] args){
+        System.out.println("Olá, Raika");
+    }
 }
